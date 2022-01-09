d6f200388ae4826dbb463e94eca5dbc6f6e3ede7
Yuu <yuu.lin@shinda.com.tw>
2022-01-09 22:14:24 +0800

    後端驗證 動態變更DOM元素

--- ShindaPratice/Controllers/ShindaController.cs
+++ ShindaPratice/Controllers/ShindaController.cs
@@ -33,67 +33,103 @@ namespace ShindaPratice.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> Post([FromBody] Sign model)
+        public ActionResult Post([FromBody] Sign model)
         {
-            TblSignup Item = new TblSignup()
+            var Str = string.Empty;
+
+            if (model.phone == "" || model.phone == null)
             {
-                CMobile = model.phone,
-                CName = model.name,
-                CEmail = model.email,
-                CCreateDt = System.DateTime.Now
-            };
+                Str += "請輸入手機號碼\n";
+            }
 
-            _context.TblSignup.Add(Item);
-            await _context.SaveChangesAsync();
-
-            var data = await _context.TblSignup.Where(x => x.CName == model.name && x.CEmail == model.email).FirstOrDefaultAsync();
-
-            var itemls = model.items.Split(',');
-
-            foreach (var item in itemls)
+            if (model.name == "" || model.name == null)
             {
-                TblSignupItem signItem = new TblSignupItem()
+                Str += "請輸入姓名\n";
+            }
+
+            if (model.email == "" || model.email == null)
+            {
+                Str += "請輸入信箱\n";
+            }
+
+            if (model.items == "")
+            {
+                Str += "請選擇至少一項\n";
+            }
+
+            if (Str != "")
+            {
+                var res = new
                 {
-                    CSignupId = data.CId,
-                    CItemId = int.Parse(item)
+                    Msg = Str,
                 };
 
-                _context.TblSignupItem.Add(signItem);
+                var resJ = JsonConvert.SerializeObject(res);
+
+                return Ok(resJ);
             }
-
-            _context.SaveChanges();
-
-            var showls = (from o in _context.TblSignup
-                          join o2 in _context.TblSignupItem
-                          on o.CId equals o2.CSignupId
-                          join o3 in _context.TblActiveItem
-                          on o2.CItemId equals o3.CItemId
-                          group o3 by new { o.CId, o2.CSignupId, o.CName, o.CEmail, o.CMobile } into g
-                          select new Sign()
-                          {
-                              Id = g.Key.CId,
-                              name = g.Key.CName,
-                              phone = g.Key.CMobile,
-                              email = g.Key.CEmail,
-                              items = ""
-                          }).ToList();
-
-            foreach (var item in showls)
+            else
             {
-                var temp = (from o2 in _context.TblSignupItem
-                            join o3 in _context.TblActiveItem
-                            on o2.CItemId equals o3.CItemId
-                            where o2.CSignupId == item.Id
-                            select o3.CItemName).ToList();
+                TblSignup Item = new TblSignup()
+                {
+                    CMobile = model.phone,
+                    CName = model.name,
+                    CEmail = model.email,
+                    CCreateDt = System.DateTime.Now
+                };
 
-                item.items = string.Join(',', temp);
+                _context.TblSignup.Add(Item);
+                _context.SaveChanges();
+
+                var data =  _context.TblSignup.Where(x => x.CName == model.name && x.CEmail == model.email).FirstOrDefault();
+
+                var itemls = model.items.Split(',');
+
+                foreach (var item in itemls)
+                {
+                    TblSignupItem signItem = new TblSignupItem()
+                    {
+                        CSignupId = data.CId,
+                        CItemId = int.Parse(item)
+                    };
+
+                    _context.TblSignupItem.Add(signItem);
+                }
+
+                _context.SaveChanges();
+
+                var showls = (from o in _context.TblSignup
+                              join o2 in _context.TblSignupItem
+                              on o.CId equals o2.CSignupId
+                              join o3 in _context.TblActiveItem
+                              on o2.CItemId equals o3.CItemId
+                              group o3 by new { o.CId, o2.CSignupId, o.CName, o.CEmail, o.CMobile } into g
+                              select new Sign()
+                              {
+                                  Id = g.Key.CId,
+                                  name = g.Key.CName,
+                                  phone = g.Key.CMobile,
+                                  email = g.Key.CEmail,
+                                  items = ""
+                              }).ToList();
+
+                foreach (var item in showls)
+                {
+                    var temp = (from o2 in _context.TblSignupItem
+                                join o3 in _context.TblActiveItem
+                                on o2.CItemId equals o3.CItemId
+                                where o2.CSignupId == item.Id
+                                select o3.CItemName).ToList();
+
+                    item.items = string.Join(',', temp);
+                }
+
+                ViewBag.show = showls.OrderByDescending(x => x.Id).ToList();
+
+                var s = JsonConvert.SerializeObject(showls, Formatting.None);
+
+                return Ok(s);
             }
-
-            ViewBag.show = showls;
-
-            var s = JsonConvert.SerializeObject(showls, Formatting.None);
-
-            return Ok(s);
         }
     }
 }
